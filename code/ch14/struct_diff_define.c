f99163dba5ae591217da3d3f5d33ee01680cebea
heaven324 <wjdtjdgh2005@gmail.com>
2023-05-27 21:29:54 +0900

    CH14 ing, p368

---
+++ code/ch14/struct_diff_define.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+struct obj {
+    char name[20];
+    int x, y;
+} Ball;
+
+
+char copy_str(char *dest, char *src_);
+int Print_Obj_Status(struct obj OBJ);
+
+
+int main() {
+    Ball.x = 3;
+    Ball.y = 4;
+    copy_str(Ball.name, "RED BALL");
+
+    Print_Obj_Status(Ball);
+
+    return 0;
+}
+
+
+int Print_Obj_Status(struct obj OBJ) {
+    printf("Location of %s \n", OBJ.name);
+    printf("( %d, %d ) \n", OBJ.x, OBJ.y);
+
+    return 0;
+}
+
+
+char copy_str(char *dest, char *src) {
+    while (*src) {
+        *dest = *src;
+        src++;
+        dest++;
+    }
+
+    *dest = '\0';
+
+    return 1;
+}
