89959c9b3abe6070e1f779ce1cb4913aa20d6b8f
heaven324 <wjdtjdgh2005@gmail.com>
2023-06-21 18:30:18 +0900

    debug

--- code/ch17/test.c
+++ code/ch17/test.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculate(int a, char *x, int b);
+int calculate(int a, char x, int b);
 
 int main(int argc, char **argv) {
     int i;
     int a = atoi(argv[1]);
     int b = atoi(argv[3]);
-    int result = calculate(a, argv[2], b);
+    int result = calculate(a, *argv[2], b);
     printf("%d \n", result);
+
+    char c[3];
+    c[0] ='*';
+    printf("%s \n", argv[2]);
 
     return 0;
 }
 
-int calculate(int a, char *x, int b) {
-    printf("char : %d \n\n", *x);
-    if (*x == 43) {
+int calculate(int a, char x, int b) {
+    printf("char : %c \n\n", x);
+    if (x == 43) {
         return a + b;
-    } else if (*x == 45) {
+    } else if (x == 45) {
         return a - b;
-    } else if (*x == 42) {
+    } else if (x == 42) {
         return a * b;
-    } else if (*x == 47) {
+    } else if (x == 47) {
         return a / b;
     }
+    else
+    return 100;
     return 0;
 }
